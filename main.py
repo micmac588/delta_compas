5d130969e102d4cea85c7da806bf0464b7cecf39
micmac588 <alebreton30@gmail.com>
2024-04-28 09:32:19 +0200

    calculate rotation speed

--- main.py
+++ main.py
@@ -2,13 +2,24 @@
 import argparse
 import logging
 import multiprocessing
+import os
 import threading
 import pynmea2
 
 from scatter_plotter import ScatterPlotter
+from tqdm import *
+from queue import Queue
 
 MIN_SPEED = 5
 INVALID_HEADING = 1000
+
+class CompassHeadingAt():
+    def __init__(self, second, heading):
+        self._second = second
+        self._heading = heading
+
+    def __str__(self):
+        return f"{self._heading} at {self._second}"
 
 def prepare_logger(logger_name, verbosity, log_file=None):
     """Initialize and set the logger.
@@ -51,42 +62,87 @@ def get_delta_heading(bottom_heading, compass_heading):
         delta_heading = 360 + delta_heading
     return delta_heading
 
+def get_rotation_speed(heading_array):
+    print(len(heading_array))
+    if len(heading_array) < 5:
+        return 999999
+    first = heading_array[0]
+    last = heading_array[len(heading_array) - 1]
+    if (type(last) == CompassHeadingAt) and  (type(first) == CompassHeadingAt):
+        if (last._second - first._second):
+            return abs((last._heading - first._heading) / (last._second - first._second))
+        else:
+            return 999999
+    else:
+        return 999999
+
 def parse_file(inputfile, queue_delta_heading):
     bottom_heading = INVALID_HEADING
     compass_heading = INVALID_HEADING
-    previous_delta_heading = INVALID_HEADING
-    previous_bottom_heading = INVALID_HEADING
+    min_delta_heading = 0
+    max_delta_heading = 0
+    rejected = 0
+    accepted = 0
+    line_counter = 0
+    min_delta_heading_at_line = 0
+    max_delta_heading_at_line = 0
+    rotation_speed = 0
     sog = 0
     fail = 0
-    with open(inputfile, "r") as fi:
-        queue_delta_heading.put(['Start', inputfile, 'delta heading', 'heading'])
-        for line in fi:
-            try:
-                msg = pynmea2.parse(line[12:])
+    seconds = 0
+    heading_array = [] * 5
+    max_rotation_speed = 0
+
+    with tqdm(total=os.path.getsize(inputfile)) as pbar:
+        with open(inputfile, "r") as fi:
+            #queue_delta_heading.put(['Start', inputfile, 'delta heading', 'heading'])
+            for line in fi:
+                line_counter +=1
+                pbar.update(len(line))
                 try:
-                    if msg.sentence_type == 'VTG':
-                        bottom_heading = msg.true_track
-                        sog = msg.spd_over_grnd_kts
-                        # print(repr(msg))
-                    if msg.sentence_type =='VHW':
-                        compass_heading = msg.heading_true  # msg.heading_magnetic
-                except Exception as e:
-                    # print(e)
+                    msg = pynmea2.parse(line[12:], check=True)
+                    try:
+                        if msg.sentence_type == 'VTG':
+                            bottom_heading = msg.true_track
+                            sog = msg.spd_over_grnd_kts
+                            # print(repr(msg))
+                        if msg.sentence_type =='VHW':
+                            compass_heading = msg.heading_true  # msg.heading_magnetic
+                            heading_array.insert(0, CompassHeadingAt(seconds, compass_heading))
+                            #print(len(heading_array))
+                        if msg.sentence_type == 'ZDA':
+                            t = msg.timestamp
+                            seconds = (t.hour * 60 + t.minute) * 60 + t.second
+                            
+                    except Exception as e:
+                        # print(e)
+                        continue
+                except pynmea2.ParseError as e:
+                    #print('Parse error: {}'.format(e))
+                    fail += 1
                     continue
-            except pynmea2.ParseError as e:
-                #print('Parse error: {}'.format(e))
-                fail += 1
-                continue
-            if sog > MIN_SPEED:  # bottom heading is not accurate when sog is too low
-                # print(f"bottom {bottom_heading} compass {compass_heading}")
-                delta_heading = get_delta_heading(int(bottom_heading), int(compass_heading))
-                if (delta_heading != previous_delta_heading) or (bottom_heading != previous_bottom_heading):
-                    previous_delta_heading = delta_heading
-                    previous_bottom_heading = bottom_heading
-                    queue_delta_heading.put([delta_heading, int(bottom_heading)])
+                rotation_speed = get_rotation_speed(heading_array)
+                if ( rotation_speed != 999999) and (rotation_speed > max_rotation_speed):
+                    max_rotation_speed = rotation_speed
+                if sog > MIN_SPEED and rotation_speed < 0.05:  # bottom heading is not accurate when sog is too low
+                    # print(f"bottom {bottom_heading} compass {compass_heading}")
+                    delta_heading = get_delta_heading(int(bottom_heading), int(compass_heading))
+                    #queue_delta_heading.put([delta_heading, int(bottom_heading)])
+                    if delta_heading > max_delta_heading:
+                        max_delta_heading = delta_heading
+                        max_delta_heading_at_line = line_counter
+                    if delta_heading < min_delta_heading:
+                        min_delta_heading = delta_heading
+                        min_delta_heading_at_line = line_counter
+                    accepted += 1
+                else:
+                    rejected +=1
+                    
 
-
-        queue_delta_heading.put(['Stop',])
+            print(f"min {min_delta_heading}, max {max_delta_heading}, \
+                  rejected {100 * rejected / (rejected + accepted)}% \
+                  min at line {min_delta_heading_at_line}, max at line {max_delta_heading_at_line}")
+            #queue_delta_heading.put(['Stop',])
 
 
 def main():
@@ -102,14 +158,14 @@ def main():
     queue_delta_heading = multiprocessing.Queue()
     plotter_delta_heading = ScatterPlotter(queue_delta_heading, logger)
 
-    plotter_delta_heading.start()
+    #plotter_delta_heading.start()
 
     logger.info(f"Start parsing of {args.inputfile}")
     parser = threading.Thread(target=parse_file, args=(args.inputfile, queue_delta_heading))
     parser.start()
     parser.join()
     logger.info(f"End of parsing {args.inputfile}")
-    plotter_delta_heading.join()
+    #plotter_delta_heading.join()
     logger.info(f"End of plotting")
 
 
